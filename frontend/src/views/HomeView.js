7d3f05e4c9a28608492efb96314c40d6bc0e0e4a
Joakim <jmalmberg.web@gmail.com>
2021-09-28 14:18:22 +0200

    Topchart moved to homeview

--- frontend/src/views/HomeView.js
+++ frontend/src/views/HomeView.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Topcoins from '../components/topcoins/Topcoins'
 
 export const HomeView = () => {
     return(
         <div>
             <h1>This is home</h1>
+            <Topcoins />
         </div>
     )
 }
