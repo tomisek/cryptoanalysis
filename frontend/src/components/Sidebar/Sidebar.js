37c7c943e73c3d27aad15fafa94089d844a9676b
Joakim <jmalmberg.web@gmail.com>
2021-10-05 15:29:58 +0200

    Added sidebar with grid

---
+++ frontend/src/components/Sidebar/Sidebar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Sidebar.css'
+
+export const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            This is sidebar
+        </div>
+    )
+}
