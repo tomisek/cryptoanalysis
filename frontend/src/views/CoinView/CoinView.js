e486e7881681d1595dc4279da104f2fb914b60b9
Joakim <jmalmberg.web@gmail.com>
2021-10-12 14:14:26 +0200

    Added forecast graph and name change

--- frontend/src/views/CoinView/CoinView.js
+++ frontend/src/views/CoinView/CoinView.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './CoinView.css'
 import CoinHistoryGraph from '../../components/CoinHistoryGraph/CoinHistoryGraph'
 import { useParams } from 'react-router-dom'
-import ForecastInfo from '../../components/ForecastInfo/ForecastInfo'
+import Forecast from '../../components/Forecast/Forecast'
 
 
 export const CoinView = () => {
@@ -13,8 +13,7 @@ export const CoinView = () => {
             <div className="coin-view">
                 <CoinHistoryGraph />
                 <div className="info-box">(Info Box component)</div>
-                <div className="prediction-graph">(Prediction Graph componet)</div>
-                <ForecastInfo />
+                <Forecast />
             </div>
         </div>
     )
