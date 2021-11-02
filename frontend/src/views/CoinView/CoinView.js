bcbb93b07d3cc7ecc07bf6318c27f0000fd72588
Tobias  Jönsson <tobbe-j93@hotmail.com>
2021-11-02 09:26:55 +0100

    fix

--- frontend/src/views/CoinView/CoinView.js
+++ frontend/src/views/CoinView/CoinView.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Route, Redirect } from 'react-router-dom';
 import './CoinView.css'
 import CoinHistoryGraph from '../../components/CoinHistoryGraph/CoinHistoryGraph'
 import { CoinInfoBox } from '../../components/CoinInfoBox/CoinInfoBox'
