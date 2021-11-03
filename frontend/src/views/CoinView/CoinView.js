0d0f464fcbd5f31ed24229add872b18ecbd24fc4
Tobias  Jönsson <tobbe-j93@hotmail.com>
2021-11-03 10:24:47 +0100

    custom loading added , forecast only for user

--- frontend/src/views/CoinView/CoinView.js
+++ frontend/src/views/CoinView/CoinView.js
@@ -1,17 +1,61 @@
-import React from 'react'
+import React,{useState, useContext, useRef} from "react";
 import './CoinView.css'
 import CoinHistoryGraph from '../../components/CoinHistoryGraph/CoinHistoryGraph'
 import { CoinInfoBox } from '../../components/CoinInfoBox/CoinInfoBox'
 import Forecast from '../../components/Forecast/Forecast'
 import SaveLastViewed from '../../components/SaveLastViewed/SaveLastViewed'
+import CryptoShuttleService from "../../utils/api/services/CryptoShuttleService";
+import { UserContext } from '../../shared/global/provider/UserProvider'
 
 
 
 
 
 export const CoinView = () => {
+    let btnRef = useRef();
     const [showResults, setShowResults] = React.useState(false)
-    const onClick = () => setShowResults(true)
+    const [authenticatedUser, setAuthenticatedUser] = useContext(UserContext)
+    let  [user, setUser] = useState()
+    const onClick = () => {setShowResults(true)
+        
+        if(btnRef.current){
+            btnRef.current.setAttribute("hidden", "hidden");
+        }
+        userAuthBackend()
+        
+    }
+        
+            
+       
+   
+    
+    
+    const userAuthBackend = async () => {
+        
+        setAuthenticatedUser(localStorage.getItem('token'))
+        console.log(authenticatedUser)
+
+        if(authenticatedUser == null){
+            console.log('login to use')
+        }else if(authenticatedUser != null) {
+                console.log(authenticatedUser)
+            try{
+                const getUserWithToken = await CryptoShuttleService.getLoggedInUser(localStorage.getItem('token'))
+                setUser(getUserWithToken.data.logged_in_as)
+                
+            }
+            catch(e){
+                console.log(e.message);
+            }
+        }
+        
+    }
+    
+   
+    
+
+
+
     
     return (
         <div className="main">
@@ -19,9 +63,11 @@ export const CoinView = () => {
                 <CoinInfoBox />
                 <CoinHistoryGraph />
                 <SaveLastViewed />
-                    
-                <input type="submit" value="Show Forecast" onClick={onClick} />
-                { showResults ? <Forecast /> : null }
+                
+                <input ref={btnRef} type="submit" value="Run Forecast" onClick={onClick} />
+                { showResults && user ? <Forecast /> : null }
+                
+                  
                 
                     
 
