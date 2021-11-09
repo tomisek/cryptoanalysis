ec5d48dc28e4ff21469f1061858e908f13f07173
Martina <a.martina.eriksson@hotmail.com>
2021-11-09 11:58:10 +0100

    show all saved forecasts

--- frontend/src/components/Userpage/SavedForecasts.js
+++ frontend/src/components/Userpage/SavedForecasts.js
@@ -1,23 +1,41 @@
 import React, {useState, useEffect} from 'react'
 import CryptoShuttleService from '../../utils/api/services/CryptoShuttleService'
+import "./SavedForecasts.css"
+
 
 export const SavedForecast = (props) => {
     const [data, setData] = useState([])
-
-    const handleClick = () =>{
-        console.log("clicked coin");
-    }
-
+    const [sortedData, setSortedData] = useState([])
+ 
     const getSavedForecasts = async () => {
         try{
             const { data } = await CryptoShuttleService.getSavedForecasts(props.userId)
             setData(data)
             console.log("data",data);
+            sortCoins(data)
 
         }
         catch(e) {
             console.log(e.message);
         }
+    }
+
+    const sortCoins = (data) => {
+        // sort by coin name asc
+        data.sort((a, b) =>  {
+            if (a.coin > b.coin){
+                return 1
+            }
+            if (a.coin < b.coin){
+                return -1
+            }
+            return 0
+        })
+        
+
+        
+        console.log("sorted",data);
+        setSortedData(data) 
     }
     useEffect(() => {
         getSavedForecasts()
@@ -26,12 +44,18 @@ export const SavedForecast = (props) => {
     return (
         <div className="savedForecast">
             <h3>Your saved forecasts</h3>
-            {Object.keys(data).map((key, index) => (
-                <div key={index}>
-                    <span onClick={handleClick}>{data[key].coin}</span>
+   
+            {Object.keys(sortedData).map((key, index) => (
+                <div key={index} className="savedForecastInfo">
+                    <div className="coinName">{sortedData[key].coin.charAt(0).toUpperCase() + sortedData[key].coin.slice(1)}</div>
+                    <div className="dateMade">Date made: {sortedData[key].date_made}</div>
+                    <div className="buyDate">Buy date: {sortedData[key].buy_date}</div>
+                    <div className="buyPrice">Buy price: {sortedData[key].buy_price}</div>
+                    <div className="sellDate">Sell date: {sortedData[key].sell_date}</div>
+                    <div className="sellPrice">Sell price: {sortedData[key].sell_price}</div>
+                    <div className="maxGainPercent">Max gain: {sortedData[key].max_gain_procent}%</div>
                 </div>
             ))}
-
         </div>
     )
 }
