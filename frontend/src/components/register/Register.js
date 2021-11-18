cc7bfbca0b48d209b1dc607641effd1ac2dd4602
Tomas <cernytomas@hotmail.com>
2021-11-18 21:01:26 +0100

    useState delete

--- frontend/src/components/register/Register.js
+++ frontend/src/components/register/Register.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react"
+import React, { useContext } from "react"
 import './Register.css'
 import useForm from "./UseForm";
 import validate from './RegisterFormValidationRules';
