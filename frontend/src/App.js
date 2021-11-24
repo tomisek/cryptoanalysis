d2a0ca24642b186b715ea5aed7558fc8c5518f2a
Tomas <cernytomas@hotmail.com>
2021-11-24 22:32:12 +0100

    signupLink

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import './shared/global/css/Global.css'
 import {NavigationBar} from './components/NavigationBar/NavigationBar'
 import { Sidebar } from './components/Sidebar/Sidebar'
 import { RegUserProvider } from './shared/global/provider/RegUserProvider'
-import { PopupsProvider } from './shared/global/provider/popupsProvider'
+import { PopupsProvider } from './shared/global/provider/PopupsProvider'
 
 
 function App() {
--- frontend/src/components/SignUpLink/SignUpLink.js
+++ frontend/src/components/SignUpLink/SignUpLink.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import { useHistory } from 'react-router-dom';
 import { LoginButton } from '../LoginButton/LoginButton';
 import "./SignUpLink.css"
+import { PopupsContext } from "../../shared/global/provider/PopupsProvider"
 
 
 export const SignUpLink = () => {
     const history = useHistory();
+    const [open, setOpen] = useContext(PopupsContext)
     
     
     
@@ -16,7 +18,7 @@ export const SignUpLink = () => {
             
                 
                 <p>New to Crypto Shuttle? </p>
-                <p onClick={() => history.push('/userregister')} className="register"> Register</p>
+                <p onClick={() => {setOpen(true)}} className="register"> Register</p>
 
                 
             
