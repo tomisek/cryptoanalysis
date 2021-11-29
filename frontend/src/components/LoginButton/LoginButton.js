90e851ef2d87987c9cb8d955478fec7a3af23b30
Tomas <cernytomas@hotmail.com>
2021-11-29 01:16:49 +0100

    hide show password core

--- frontend/src/components/LoginButton/LoginButton.js
+++ frontend/src/components/LoginButton/LoginButton.js
@@ -4,72 +4,76 @@ import Popup from "reactjs-popup"
 import { UserContext } from "../../shared/global/provider/UserProvider"
 import CryptoShuttleService from "../../utils/api/services/CryptoShuttleService"
 import { PopupsContext } from "../../shared/global/provider/PopupsProvider"
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye } from '@fortawesome/free-solid-svg-icons'
+import { faEyeSlash } from '@fortawesome/free-solid-svg-icons'
 
 
 export const LoginButton = () => {
- 
+
     const [email, setEmail] = useState()
     const [password, setPassword] = useState()
     const [authenticatedUser, setAuthenticatedUser] = useContext(UserContext)
-
     const [open, setOpen] = useContext(PopupsContext)
-    
+    const [passwordShown, setPasswordShown] = useState(false)
+    const eye = <FontAwesomeIcon icon={faEye} />;
+    const eyeSlash = <FontAwesomeIcon icon={faEyeSlash} />;
 
-    
-    
-    const handleSubmit = async (event)   => {
+    const togglePasswordVisiblity = () => {
+        setPasswordShown(passwordShown ? false : true);
+    };
+
+    const handleSubmit = async (event) => {
         event.preventDefault()
 
-       try{
-        const userFromServer = await CryptoShuttleService.loginUser({email, password})
-        setAuthenticatedUser(userFromServer.data.access_token)
-        localStorage.setItem("token", userFromServer.data.access_token)
-        let btn = document.getElementById('forecastBtn')
-        if(btn){
+        try {
+            const userFromServer = await CryptoShuttleService.loginUser({ email, password })
+            setAuthenticatedUser(userFromServer.data.access_token)
+            localStorage.setItem("token", userFromServer.data.access_token)
+            let btn = document.getElementById('forecastBtn')
+            if (btn) {
 
-            btn.removeAttribute("hidden")
-        }
+                btn.removeAttribute("hidden")
+            }
 
         }
-        catch(error){
+        catch (error) {
             document.getElementById("noMatch").style.visibility = "visible"
-        }        
+        }
     }
-    
+
     return (
         <div >
-            <Popup  trigger={<button className="triggerButton"> Login</button>} modal >
+            <Popup trigger={<button className="triggerButton"> Login</button>} modal >
                 {close => (
                     <div>
-                    <form className="wrapper" onSubmit={handleSubmit}>
-                        <button className="close" onClick={close}>
-                        &times;
-                        </button>
-                        <h4>Log In</h4>
-                        <div>
-                            New to Crypto?
-                            <span className="create-account" onClick={() => {close(); setOpen(true) }}> Create an account.</span>
+                        <form className="wrapper" onSubmit={handleSubmit}>
+                            <button className="close" onClick={close}>
+                                &times;
+                            </button>
+                            <h4>Log In</h4>
+                            <div>
+                                New to Crypto?
+                                <span className="create-account" onClick={() => { close(); setOpen(true) }}> Create an account.</span>
+                            </div><br />
+
+                            <div>
+                                <label ><b>Email</b></label><br />
+                                <input onChange={event => setEmail(event.target.value)} type="email" className="row" placeholder="Enter Email" name="email" required></input>
+                            </div><br />
+                            <div>
+                                <label><b>Password</b></label><br />
+                                <i className="eye-icon-login" onClick={togglePasswordVisiblity}>{passwordShown ? eyeSlash : eye}</i>
+                                <input onChange={event => setPassword(event.target.value)} type={passwordShown ? "text" : "password"} className="row" placeholder="Enter Password" name="psw" required></input>
                             </div>
-                        
-                        <div>
-                        <label ><b>Email</b></label><br/>
-                        <input onChange={event => setEmail(event.target.value)} type="email" className="row" placeholder="Enter Email" name="email" required></input>
-                        </div><br/>
-                        <div>
-                        <label><b>Password</b></label><br/>
-                        <input onChange={event => setPassword(event.target.value)} type="password" className="row" placeholder="Enter Password" name="psw" required></input>
-                        
-                        <div id="noMatch">Your email and password doesnt match. Please try again</div>
-                        </div><br/>
-                        <div>
-                        <button type="submit" className="submit">Submit</button> 
-                        </div>
-                    </form>
-                </div>
+                            <div id="noMatch">Your email and password doesnt match. Please try again</div>
+                            <div>
+                                <button type="submit" className="submit">Submit</button>
+                            </div>
+                        </form>
+                    </div>
                 )}
-                
-            </Popup>        
+            </Popup>
         </div>
     )
 }
--- frontend/src/components/register/Register.js
+++ frontend/src/components/register/Register.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react"
+import React, { useContext, useState } from "react"
 import './Register.css'
 import useForm from "./UseForm";
 import validate from './RegisterFormValidationRules';
@@ -7,97 +7,117 @@ import { UserContext } from "../../shared/global/provider/UserProvider"
 import { RegUserContext } from "../../shared/global/provider/RegUserProvider";
 import Popup from "reactjs-popup";
 import { PopupsContext } from "../../shared/global/provider/PopupsProvider";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye } from '@fortawesome/free-solid-svg-icons'
+import { faEyeSlash } from '@fortawesome/free-solid-svg-icons'
 
 export const Register = (props) => {
 
     const [authenticatedUser, setAuthenticatedUser] = useContext(UserContext)
     const [registerError, setRegisterError] = useContext(RegUserContext)
-    /* const [registerError, setRegisterError] = useState() */
     const [open, setOpen] = useContext(PopupsContext);
     const closeModal = () => setOpen(false);
+    const [passwordShown, setPasswordShown] = useState(false)
+    const eye = <FontAwesomeIcon icon={faEye} />;
+    const eyeSlash = <FontAwesomeIcon icon={faEyeSlash} />;
+
+    const togglePasswordVisiblity = () => {
+        setPasswordShown(passwordShown ? false : true);
+    };
+
     const {
         values,
         errors,
         handleChange,
         handleSubmit,
-      } = useForm(login, validate);
+    } = useForm(login, validate);
 
-      const logInUser = async ()   => {
-         
-         const email = values.email
-         const password = values.password
-         const userFromServer = await CryptoShuttleService.loginUser({email, password})
-         setAuthenticatedUser(userFromServer.data.access_token)
-         localStorage.setItem("token", userFromServer.data.access_token)
-         }
-     
 
-      //registering user
-      async function  login (e) {
-        if(e) e.preventDefault()
+
+    const logInUser = async () => {
+
+        const email = values.email
+        const password = values.password
+        const userFromServer = await CryptoShuttleService.loginUser({ email, password })
+        setAuthenticatedUser(userFromServer.data.access_token)
+        localStorage.setItem("token", userFromServer.data.access_token)
+    }
+
+
+    //registering user
+    async function login(e) {
+        if (e) e.preventDefault()
         console.log('No errors, submit callback called!');
-        
-        
+
+
         const userObject = {
             "email": values.email,
             "name": values.name,
             "password": values.password,
         }
-        
-        try{
+
+        try {
             const response = await CryptoShuttleService.registerUser(userObject)
             console.log(response);
             logInUser();
             closeModal()
         }
-        catch(error){
-            if (error.response.data){
+        catch (error) {
+            if (error.response.data) {
                 // Show error message from server
                 setRegisterError(error.response.data['error'])
                 document.getElementById("noMatch").style.visibility = "visible"
             }
-      
+
             console.error(error.message);
         }
-      };
-    
+    };
+
     return (
-        
-            
-            <div>
+
+
+        <div>
             <button type="button" className="registerButton" onClick={() => setOpen(o => !o)}>
-            Sign Up
+                Sign Up
             </button>
             <Popup open={open} closeOnDocumentClick onClose={closeModal}>
-            <div className="wrapper">
+                <div className="wrapper">
                     <button className="closeRegisterModal" onClick={closeModal}>
-                    &times;
+                        &times;
                     </button>
                     <h4>Register</h4>
                     <form onSubmit={handleSubmit} noValidate>
-        
-                    <label htmlFor="email">Email</label><input autoComplete="off" className={`input ${errors.email}`} type="email" name="email" onChange={handleChange} value={values.email || ''} required />
-                    {errors.email && (
-                    <p className="help">{errors.email}</p>
-                    )}
-                    <label htmlFor="name">Name</label><input autoComplete="off" className={`input ${errors.name}`} type="text" name="name" onChange={handleChange} value={values.name || ''} required />
-                    {errors.name && (
-                    <p className="help">{errors.name}</p>
-                    )}
-                    <label htmlFor="password">Password</label> <input className={`input ${errors.password}`} type="password" name="password" onChange={handleChange} value={values.password || ''} required />
-                
-                    {errors.password && (
-                    <p className="help">{errors.password}</p>
-                    )}
-                    <label htmlFor="password">Confirm Password</label><input className={`input ${errors.confirmPassword}`} type="password" name="confirmPassword" onChange={handleChange} value={values.confirmPassword || ''} required />
-                    {errors.confirmPassword && (
-                    <p className="help">{errors.confirmPassword}</p>
-                    )}
-                    <div id="noMatch">{registerError}</div>
-                    <button type="submit" className="regSubmit">Register</button>
-                </form>
-            </div>
-            
+                        <div>
+                            <label htmlFor="email">Email</label><input autoComplete="off" className={`input  ${errors.email}`} type="email" name="email" onChange={handleChange} value={values.email || ''} required />
+                            {errors.email && (
+                                <p className="help">{errors.email}</p>
+                            )}
+                        </div>
+                        <div>
+                            <label htmlFor="name">Name</label><input autoComplete="off" className={`input ${errors.name}`} type="text" name="name" onChange={handleChange} value={values.name || ''} required />
+                            {errors.name && (
+                                <p className="help">{errors.name}</p>
+                            )}
+                        </div>
+                        <div>
+                            <label htmlFor="password">Password</label>
+                            <i className="eye-icon-register" onClick={togglePasswordVisiblity}>{passwordShown ? eyeSlash : eye}</i>
+                            <input className={`input ${errors.password}`} type={passwordShown ? "text" : "password"} name="password" onChange={handleChange} value={values.password || ''} required />
+                            {errors.password && (
+                                <p className="help">{errors.password}</p>
+                            )}
+                        </div>
+                        <div>
+                            <label htmlFor="password">Confirm Password</label>
+                            <input className={`input ${errors.confirmPassword}`} type="password" name="confirmPassword" onChange={handleChange} value={values.confirmPassword || ''} required />
+                            {errors.confirmPassword && (
+                                <p className="help">{errors.confirmPassword}</p>
+                            )}
+                        </div>
+                        <div id="noMatch">{registerError}</div>
+                        <button type="submit" className="regSubmit">Register</button>
+                    </form>
+                </div>
             </Popup>
         </div>
     )
