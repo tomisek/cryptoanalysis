d109c48427e107acbdbd5cb4b8972a9ede343d22
Joakim <jmalmberg.web@gmail.com>
2021-11-29 11:00:57 +0100

    Style change for global market info

--- frontend/src/components/GlobalMarketInfo/GlobalMarketInfo.js
+++ frontend/src/components/GlobalMarketInfo/GlobalMarketInfo.js
@@ -43,13 +43,13 @@ import { faLevelUpAlt, faLevelDownAlt } from '@fortawesome/free-solid-svg-icons'
                         
             <div className="global-market-info">                                        
                 <div className="market-info">   
-                    <div>Active cryptos: {globalMarketInfo.active_cryptocurrencies}</div>
-                    <div>Market Cap: ${globalMarketInfo.total_market_cap}</div>
+                    <div>Active cryptos: {globalMarketInfo.active_cryptocurrencies ? globalMarketInfo.active_cryptocurrencies.toLocaleString() : 'N/A'}</div>
+                    <div>Market Cap: ${globalMarketInfo.total_market_cap ? globalMarketInfo.total_market_cap.toLocaleString() : 'N/A'}</div>
                     <div className="cap-percentage"> Market Change: 
                         <div className={(globalMarketInfo.market_change_percentage < 0) ? 'negative' : 'positive'}>{globalMarketInfo.market_change_percentage}% <FontAwesomeIcon icon={(globalMarketInfo.market_change_percentage < 0) ? faLevelDownAlt : faLevelUpAlt}/></div>                                                                                                                 
                     </div>    
                     <div>Exchanges: {globalMarketInfo.total_markets}</div>
-                    <div>24h-Volume: ${globalMarketInfo.total_volume}</div>
+                    <div>24h-Volume: ${globalMarketInfo.total_volume ? globalMarketInfo.total_volume.toLocaleString() : 'N/A'}</div>
                     <div className="dominance"> 
                         {Object.keys(capPercentage).map((key, index) => {                                   
                             return <div key={index}>                                        
