6fffcb2e07f752643010b5c173ebf67f05e7ac13
Tobias  Jönsson <tobbe-j93@hotmail.com>
2021-12-02 11:14:48 +0100

    lastviewed updating on page change

--- frontend/src/components/LastViewed/LastViewed.js
+++ frontend/src/components/LastViewed/LastViewed.js
@@ -4,29 +4,38 @@ import './LastViewed.css'
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faClock } from '@fortawesome/free-regular-svg-icons'
-
+import { useHistory } from 'react-router-dom';
 
 export const LastViewed = () => {
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
     const [lastViewed, setLastViewed] = useState([]);
     const lastViewedCoins = JSON.parse(localStorage.getItem("lastViewedCoins"));
+    const history = useHistory()
     
-    const fetchData = async () => {
+    const fetchData = async (props) => {
         try {
-            const { data } = await CryptoShuttleService.lastViewed(lastViewedCoins)
+            const { data } = await CryptoShuttleService.lastViewed(props)
             setIsLoaded(true);
             setLastViewed(data);
         } catch (error) {
             setIsLoaded(true);
             setError(error);
         }
-    }
-
+    } 
     useEffect(() => {
-        fetchData()
-    }, [])
-
+         
+        return history.listen((location) => {
+            
+            let routeC = location.pathname
+            if(location !== routeC) {
+                
+                let c = JSON.parse(localStorage.getItem('lastViewedCoins'))
+                fetchData(c)
+            }
+        }) 
+    }, [history]);
+                
     const mapOrder = (array, order, key) => {
         array.sort(function (a, b) {
             var A = a[key], B = b[key];
