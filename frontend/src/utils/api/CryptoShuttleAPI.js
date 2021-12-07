4dfe84fd734f9ae4e7b330ee81ae43e9177e47f3
tomisek <cernytomas@hotmail.com>
2021-12-07 23:15:30 +0100

    second commit

--- frontend/src/utils/api/CryptoShuttleAPI.js
+++ frontend/src/utils/api/CryptoShuttleAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const development = "http://127.0.0.1:5000"
+/* const development = "http://127.0.0.1:5000" */
 const production = "https://obscure-cliffs-37126.herokuapp.com/"
 
 const CryptoShuttleAPI = axios.create({
