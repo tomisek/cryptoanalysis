c9a1c7d2ba596bc100300252067d1d5e302ddac9
tomisek <cernytomas@hotmail.com>
2021-12-20 13:19:05 +0100

    Updates

--- JSONencoder.py
+++
@@ -1,9 +0,0 @@
-from bson.json_util import ObjectId
-import json
-
-class MyEncoder(json.JSONEncoder):
-
-    def default(self, obj):
-        if isinstance(obj, ObjectId):
-            return str(obj)
-        return super(MyEncoder, self).default(obj)
